2f45d42b5a491c183161bcad42fd330cfc8bf315
Jonathan <jdomingo-2022206@kinal.edu.gt>
2024-07-14 19:19:38 -0600

    New version 1.1.

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-config();
-
-import Server from "./configs/server.js";
-// Crear una instancia del servidor
-const server = new Server();
-// Iniciar el servidor
-server.listen();
